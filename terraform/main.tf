0850b85907f9abbd2e6ebba7aea5064fab09e605
Suba Shree <d.subashree94@gmail.com>
2026-05-11 19:16:25 +0530

    Initial DevOps project setup

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+  key_name      = "jenkins-key"
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
